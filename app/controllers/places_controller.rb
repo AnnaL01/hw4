ef71624e17af58d4f8227e372c2c5004f8288772
AnnaL01 Lu <anna.lu@kellogg.northwestern.edu>
2024-06-05 07:35:32 +0000

    commit

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,22 +1,24 @@
 class PlacesController < ApplicationController
 
-  def index
-    @places = Place.all
-  end
+  class PlacesController < ApplicationController
 
-  def show
-    @place = Place.find_by({ "id" => params["id"] })
-    @entries = Entry.where({ "place_id" => @place["id"] })
+    def index
+      @places = Place.all
+    end
+  
+    def show
+      @place = Place.find_by({ "id" => params["id"] })
+      @entries = Entry.where({ "place_id" => @place["id"], "user_id" => session["user_id"] })
+    end
+  
+    def new
+    end
+  
+    def create
+      @place = Place.new
+      @place["name"] = params["name"]
+      @place.save
+      redirect_to "/places"
+    end
+  
   end
-
-  def new
-  end
-
-  def create
-    @place = Place.new
-    @place["name"] = params["name"]
-    @place.save
-    redirect_to "/places"
-  end
-
-end
